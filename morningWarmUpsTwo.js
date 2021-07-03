df520d7b98e4c95b7939b3565b3b68f84057c8e4
johnmccloskey <john.j.mccloskey76@gmail.com>
2021-07-03 15:33:52 -0500

    Study session #1 with Trevor Mullis 7-2-21

---
+++ morningWarmUpsTwo.js
@@ -0,0 +1,27 @@
+// TODO: Create a function named countSpaces that accepts a string and
+//  returns the total number of spaces in the string.
+
+function countSpaces(str) {
+    if (typeof str !== 'string'){
+        return false;
+    }
+    else {
+        let spaceCount = 0; //(str.split(" ").length - 1);
+        for (let x = 0; x < str.length; x++) {
+           if (str.substring(x, x + 1) === " ") {
+               spaceCount++;
+           }
+
+        }
+        return spaceCount;
+    }
+
+
+    //console.log(spaceCount);
+
+}
+console.log(countSpaces("Total number of spaces"));
+console.log(countSpaces("Total number"));
+console.log(countSpaces("Total number of spaces in the United States of America"));
+
+// TODO: Add validation to function above, if the argument pass is not a string it should return false.
